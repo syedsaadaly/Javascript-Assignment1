9e0b02e8fd6f18d3c14676a4fb7e4ed9f553c292
Saad ali <142331196+syedsaadaly@users.noreply.github.com>
2024-02-27 21:44:52 +0500

    Add files via upload

---
+++ Javascript Assignment/chapter 1/app.js
@@ -0,0 +1,46 @@
+// q1 Write a script to greet your website visitor using JS alert
+// box.
+
+// alert("Welcome To My Website")
+// Swal.fire("Welcome To My Website");
+
+
+// q2 Write a script to display following message on your web
+// page:
+
+// alert("Error! please enter a valid password")
+// Swal.fire("Error! please enter a valid password")
+
+
+// q3 Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+
+// alert("Welcome To JS Land... \n Happy Coding")
+// Swal.fire("Welcome To JS Land... \n Happy Coding")
+
+// q4
+
+
+// alert("Welcome To JS Land... ")
+// alert("Happy Coding \n prevent this page to creating additional dialouges")
+// Swal.fire("Welcome To JS Land... ")
+// Swal.fire("Happy Coding \n prevent this page to creating additional dialouges")
+
+
+// q5
+
+// alert("Heloo.... i can run js through my web browsers console")
+// Swal.fire("Heloo.... i can run js through my web browsers console")
+
+
+
+
+
+// q6
+
+
+// alert("Welcome To My Website ")
+// Swal.fire("Welcome To My Website")
+
+
